b92f16aabd29d0963431dc0e16c940406cf0a0c7
Olivencia <cristoly94@gmail.com>
2016-05-31 14:36:04 +0200

    Keep process

--- public_html/tiendamusica/routes/index.js
+++ public_html/tiendamusica/routes/index.js
@@ -4,32 +4,37 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('template', { title: 'DiscoShop', prueba: 'eii', page: 'home' });
+  res.render('template', { title: 'DiscoShop', prueba: 'hola', page: 'home' });
 });
 
 //GET Rock category page. 
 router.get('/rock', function(req, res, next) {
-  res.render('template', { title: 'rock', prueba: 'eii', page: 'category' });
+  res.render('template', { title: 'rock', prueba: 'hola', page: 'category' });
 });
 
 //GET Rap category page. 
 router.get('/rap', function(req, res, next) {
-  res.render('template', { title: 'rap', prueba: 'eii', page: 'category' });
+  res.render('template', { title: 'rap', prueba: 'hola', page: 'category' });
 });
 
 //GET Metal category page. 
 router.get('/metal', function(req, res, next) {
-  res.render('template', { title: 'metal', prueba: 'eii', page: 'category' });
+  res.render('template', { title: 'metal', prueba: 'hola', page: 'category' });
 });
 
 //GET pop category page. 
 router.get('/pop', function(req, res, next) {
-  res.render('template', { title: 'pop', prueba: 'eii', page: 'category' });
+  res.render('template', { title: 'pop', prueba: 'hola', page: 'category' });
 });
 
 //GET pop category page. 
 router.get('/producto', function(req, res, next) {
-  res.render('template', { title: 'producto', prueba: 'eii', page: 'product' });
+  res.render('template', { title: 'producto', prueba: 'hola', page: 'product' });
+});
+
+//GET pop category page. 
+router.get('/subscripcion', function(req, res, next) {
+  res.render('template', { title: 'subscripcion', prueba: 'hola', page: 'subs' });
 });
 
 module.exports = router;
