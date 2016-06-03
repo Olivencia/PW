6269ed96c4173b58ccd8f6e8ff13eadf384401f9
Olivencia <cristoly94@gmail.com>
2016-06-03 20:55:10 +0200

    Write discs to Mongo database

--- public_html/tiendamusica/routes/index.js
+++ public_html/tiendamusica/routes/index.js
@@ -1,43 +1,46 @@
 var express = require('express');
 var router = express.Router();
-
 var url = require('url');
+var jQuery = require('jquery');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
 
 
-var mysql = require('mysql');
+function insertData(json_data, collect) {
+  var db_url = 'mongodb://localhost:27017/tienda';
+  MongoClient.connect(db_url, function(err, db) {
+    assert.equal(null, err);
+    insertDocument(db, function() {
+        db.close();
+    }, collect, json_data);
+  });
+};
 
-var connection = mysql.createConnection({
-   host: 'localhost',
-   user: 'shop_admin',
-   database: 'tiendamusica',
-   port: 3306
-});
-connection.connect(function(error){
-   if(error){
-      console.log('Conexion incorrecta.');
-   }else{
-      console.log('Conexion correcta.');
-   }
-});
+var insertDocument = function(db, callback, collect, json_data) {
+   db.collection(collect).insertOne(JSON.parse(JSON.stringify(json_data)), function(err, result) {
+    assert.equal(err, null);
+    console.log("Data inserted in database");
+    callback();
+  });
+};
+
 
 function decodeParams(params) {
     var p = params.split("&");
     if(typeof p == "undefined")
-    	p = params;
-    var temp = new Array();
+      p = params;
+    var temp = {};
     var cnt = 0;
     for(var i=0; i<p.length; i++){
-    	temp[cnt] = p[i].split("=")[0];
-    	temp[cnt+1] = p[i].split("=")[1];
-    	cnt+=2;
+      temp[p[i].split("=")[0]] = p[i].split("=")[1];
+      cnt+=2;
     }
     return temp;
 }
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	params = req.url;
-	console.log(params);
+  params = req.url;
+  console.log(params);
   res.render('template', { title: 'DiscoShop', prueba: 'hola', page: 'home' });
 });
 
@@ -68,17 +71,26 @@ router.get('/producto', function(req, res, next) {
 
 //GET subscripcion page. 
 router.get('/subscripcion', function(req, res, next) {
-	var url_params = req.url.split("?")[1];
-	if( typeof url_params !== "undefined" ){
-		var params = decodeParams(url_params);
-	}
-	
+  var url_params = req.url.split("?")[1];
+  if( typeof url_params !== "undefined" ){
+    var params = decodeParams(url_params);
+    var json_data = arrayToJSON(params);
+    //insertData(json_data);
+  }
+  
   res.render('template', { title: 'subscripcion', prueba: 'hola', page: 'subs' });
 });
 
 //GET admin page. 
 router.get('/admin', function(req, res, next) {
+  var url_params = req.url.split("?")[1];
+  if( typeof url_params !== "undefined" ){
+    var json_data = decodeParams(url_params);
+    insertData(json_data, 'discs');
+    //console.log(json_data);
+  }
   res.render('template', { title: 'admin', prueba: 'hola', page: 'admin' });
 });
+
 
 module.exports = router;
