6ec3344fd14536ec5260c5f103050f7d9b10e41f
Olivencia <cristoly94@gmail.com>
2016-06-03 20:53:58 +0200

    JQuery script added

---
+++ public_html/tiendamusica/public/javascripts/view.index.js
@@ -0,0 +1,18 @@
+$(function(){
+  $('#submit-button').on("click", function(){
+      $('#success').css("display","block");
+      console.log("hola");
+  });
+
+  $('#nav-bar-genre').on("mouseover", function(){
+      console.log("hola");
+  });
+
+  $('#nav-bar-artists').on("mouseover", function(){
+      console.log("hola");
+  });
+
+  $('#nav-bar-lasts-discs').on("mouseover", function(){
+      console.log("hola");
+  });
+});
