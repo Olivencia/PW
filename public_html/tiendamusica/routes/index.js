1b1ff65d09ddb04869435e172cf7826764b927dc
Olivencia <cristoly94@gmail.com>
2016-06-04 20:31:41 +0200

    Category page done

--- public_html/tiendamusica/routes/index.js
+++ public_html/tiendamusica/routes/index.js
@@ -19,35 +19,57 @@ function decodeParams(params) {
     return temp;
 }
 
+function encodeParams(json){
+  var params = '?';
+  for (data in json){
+    if(data !== '_id') params += data + '=' + json[data] + '&';
+  }
+  params[params.length-1] = '';
+  return params;
+}
+
 /* home page. */
 router.get('/', function(req, res, next) {
   params = req.url;
   //console.log(params);
-  res.render('template', { title: 'DiscoShop', prueba: 'hola', page: 'home' });
+  res.render('template', { title: 'DiscoShop', page: 'home' });
 });
 
-/* Rock category page.*/ 
+/* category page.*/ 
 router.get('/category', function(req, res, next) {
-  //console.log(decodeParams(req.url.split("?")[1]));
   var params = decodeParams(req.url.split("?")[1]);
   var discs = db.get().collection('discs');
-  var cat = db.get().collection('discs');
-  cat.find({genre: params.cat}).toArray(function(err, docs) {
+  discs.find({genre: params.cat}).toArray(function(err, cat_docs) {
       if(err)
       console.log("Cant find at DB");
     else{
-      docs.forEach(function(doc){
+      var feature_doc = cat_docs[0];
+      cat_docs.splice(0,1);
+      cat_docs.forEach(function(doc){
+        doc.url = 'http://localhost:3000/product' + encodeParams(doc);
         doc.title = doc.title.split('+').join(' ');
         doc.author = doc.author.split('+').join(' ');
       });
-    res.render('template', { title: 'Sección ' + params.cat.toUpperCase(), prueba: 'hola', discs: docs , page: 'category' });
+
+      discs.find().limit(5).toArray(function(err, more_seller_docs) {
+        if(err)
+          console.log("Cant find at DB");
+        else{
+          more_seller_docs.forEach(function(doc){
+            doc.url = 'http://localhost:3000/product' + encodeParams(doc);
+            doc.title = doc.title.split('+').join(' ');
+            doc.author = doc.author.split('+').join(' ');
+          });
+          res.render('template', { title: 'Sección ' + params.cat.toUpperCase(), feature_disc: feature_doc, cat_discs: cat_docs, more_seller_discs: more_seller_docs,  page: 'category' });
+        }
+      });
     }
   });
 
 });
 
 /* product page. */
-router.get('/producto', function(req, res, next) {
+router.get('/product', function(req, res, next) {
 
   var params = decodeParams(req.url.split("?")[1]);
   var discs = db.get().collection('discs');
@@ -57,15 +79,23 @@ router.get('/producto', function(req, res, next) {
       console.log("Cant find at DB");
     else{
       console.log(docs[2]);
-      res.render('template', { title: 'producto', prueba: 'hola', page: 'product' });
+      res.render('template', { title: 'product', page: 'product' });
     }
   });
 });
 
 /* subscripcion page. */
 router.get('/subscripcion', function(req, res, next) {
-  writeData(req.url.split("?")[1], 'users');
-  res.render('template', { title: 'subscripcion', prueba: 'hola', page: 'subs' });
+  var json_data = decodeParams(req.url.split("?")[1]);
+  var collection = db.get().collection('users');
+  collection.insertOne(JSON.parse(JSON.stringify(json_data)), function(err, result) {
+    if(err)
+      console.log("Cant insertOne to DB");
+    else{
+      console.log("Insert to database");
+      res.render('template', { title: 'subscripcion', page: 'subs' });
+    }
+  });
 });
 
 /* admin page. */
@@ -77,7 +107,7 @@ router.get('/admin', function(req, res, next) {
       console.log("Cant insertOne to DB");
     else{
       console.log("Insert to database");
-      res.render('template', { title: 'admin', prueba: 'hola', page: 'admin' });
+      res.render('template', { title: 'admin', page: 'admin' });
     }
   });
 });
